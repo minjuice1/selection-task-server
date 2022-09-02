59fb1b88c08882b5db830478bf45eb1670ca29b6
minjuice1 <minjuice93@gmail.com>
2022-09-02 18:16:46 +0900

    Chapter 09 : MVC REST API

--- 05/routes/api/employees.js
+++ 05/routes/api/employees.js
@@ -1,33 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const data = {};
-data.employees = require("../../data/employees.json");
+const employeesController = require("../../controllers/employeesController");
+const {
+	getAllEmployees,
+	createNewEmployees,
+	updateEmployees,
+	deleteEmployees,
+	getEmployee,
+} = employeesController;
 
 router
 	.route("/") //
-	.get((req, res) => {
-		res.json(data.employees);
-	})
-	.post((req, res) => {
-		res.json({
-			firstname: req.body.firstname,
-			lastname: req.body.lastname,
-		});
-	})
-	.put((req, res) => {
-		res.json({
-			firstname: req.body.firstname,
-			lastname: req.body.lastname,
-		});
-	})
-	.delete((req, res) => {
-		res.json({ id: req.body.id });
-	});
+	.get(getAllEmployees)
+	.post(createNewEmployees)
+	.put(updateEmployees)
+	.delete(deleteEmployees);
 
 router
 	.route("/:id") //
-	.get((req, res) => {
-		res.json({ id: req.params.id });
-	});
+	.get(getEmployee);
 
 module.exports = router;
